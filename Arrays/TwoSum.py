adc4eb61f257e361de2d882b5f3dabc0fa24fa73
AminKhosusimehr <khosusimehr@gmail.com>
2025-10-12 18:45:36 +0330

    The answer to two sum problem

---
+++ Arrays/TwoSum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums, target):
+        num_map = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_map:
+                return [num_map[complement], i]
+            num_map[num] = i
+        return []  
+
+s = Solution()
+s.twoSum([3,2,4] , 6)        
